70d7d7d38a892375c167350a115ce09dae8f8415
Bavithran <sivabavithran16@gmail.com>
2024-12-27 17:49:11 +0530

    backend database connected and server setup

--- backend/Server.js
+++ backend/Server.js
@@ -1,2 +1,13 @@
-export default function (){
-}
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const app = express();
+
+dotenv.config();
+connectDB();
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log("Server is listening");
+});
