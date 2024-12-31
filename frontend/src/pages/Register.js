3e42b3643d4d01b23288bc4f3cd52d14ef851645
Bavithran <sivabavithran16@gmail.com>
2024-12-31 21:11:14 +0530

    register frontend design

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -13,6 +13,7 @@ const Register = () => {
     profile: null,
   });
 
+  const [preview, setPreiview] = useState(null);
   const [errors, setErrors] = useState({});
   const navigate = useNavigate();
 
@@ -29,8 +30,10 @@ const Register = () => {
       setErrors({ ...errors, profile: "File Must be PNG or JPEG Image" });
     } else {
       setFormData({ ...formData, profile: file });
+      setPreiview(URL.createObjectURL(file));
     }
   };
+  const handleGoogleSignIn = () => {};
 
   const handleKeyPress = (e) => {
     // Allow only letters and spaces in the name field
@@ -112,72 +115,152 @@ const Register = () => {
   };
 
   return (
-    <div>
-      <h1>Register</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="name"
-          placeholder="Name"
-          value={formData.name}
-          onKeyPress={handleKeyPress}
-          onChange={handleChange}
-          required
-        />
+    <div
+      className="relative min-h-screen bg-cover bg-center"
+      style={{ backgroundImage: "url('/assets/images/Signin_background.png')" }}
+    >
+      <div className="font-lora container mx-auto w-full max-w-4xl min-h-screen flex flex-col items-center justify-center p-16">
+        <div className="relative w-full h-auto bg-white bg-opacity-20 backdrop-blur-lg rounded-3xl shadow-lg p-6 flex flex-col gap-6">
+          {preview && (
+            <div className="absolute top-4 right-4">
+              <img
+                src={preview}
+                alt="Preview"
+                className="w-24 h-24 rounded-full border-4 border-cyan-700 shadow-lg"
+              />
+            </div>
+          )}
 
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          value={formData.email}
-          onChange={handleChange}
-        />
-        {errors.email && <p className="error-message">{errors.email}</p>}
+          <h1 className="text-center text-5xl text-blue-800 font-extrabold tracking-wider mb-5">
+            SignIn
+          </h1>
 
-        <input
-          type="text"
-          name="address"
-          placeholder="Address"
-          value={formData.address}
-          onKeyPress={handleKeyPress}
-          onChange={handleChange}
-        />
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div>
+              
+              <form onSubmit={handleSubmit} className="flex flex-col gap-4" id="registrationForm">
+                <label className="font-medium text-lg ">Name:</label>
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Name"
+                  value={formData.name}
+                  onKeyPress={handleKeyPress}
+                  onChange={handleChange}
+                  required
+                  className="placeholder:text-gray-500 placeholder:tracking-wide placeholder:text-base p-3 border bg-white bg-opacity-40 backdrop-blur-lg border-gray-200 shadow-lg rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
 
-        <input
-          type="text"
-          name="phone"
-          placeholder="Contact Number"
-          onKeyPress={handleKeyPress}
-          value={formData.phone}
-          onChange={handleChange}
-        />
-        {errors.phone && <p className="error-message">{errors.phone}</p>}
+                <label className="font-medium text-lg">Address:</label>
+                <input
+                  type="text"
+                  name="address"
+                  placeholder="Address"
+                  value={formData.address}
+                  onKeyPress={handleKeyPress}
+                  onChange={handleChange}
+                  className="placeholder:text-gray-500 placeholder:tracking-wide placeholder:text-base p-3 border bg-white bg-opacity-40 backdrop-blur-lg border-gray-200 shadow-lg rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
 
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          value={formData.password}
-          onChange={handleChange}
-        />
-        {errors.password && <p className="error-message">{errors.password}</p>}
+                <label className="font-medium text-lg">Contact No:</label>
+                <input
+                  type="text"
+                  name="phone"
+                  placeholder="Contact Number"
+                  onKeyPress={handleKeyPress}
+                  value={formData.phone}
+                  onChange={handleChange}
+                  className="placeholder:text-gray-500 placeholder:tracking-wide placeholder:text-base p-3 border bg-white bg-opacity-40 backdrop-blur-lg border-gray-200 shadow-lg rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+                {errors.phone && (
+                  <p className="text-red-500 text-sm">{errors.phone}</p>
+                )}
+                <label className="text-xl font-bold">Password:</label>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  className="placeholder:tracking-wide placeholder:text-base p-3 border bg-white bg-opacity-40 backdrop-blur-lg border-gray-200 shadow-lg rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 placeholder:text-white"
+                />
+                {errors.password && (
+                  <p className="text-red-500 text-sm">{errors.password}</p>
+                )}
 
-        <input
-          type="password"
-          name="confirmPassword"
-          placeholder="Confirm Password"
-          value={formData.confirmPassword}
-          onChange={handleChange}
-        />
-        {errors.confirmPassword && (
-          <p className="error-message">{errors.confirmPassword}</p>
-        )}
+                <input
+                  type="password"
+                  name="confirmPassword"
+                  placeholder="Confirm Password"
+                  value={formData.confirmPassword}
+                  onChange={handleChange}
+                  className="placeholder:tracking-wide placeholder:text-base placeholder:text-white p-3 border bg-white bg-opacity-40 backdrop-blur-lg border-gray-200 shadow-lg rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 mb-5"
+                />
+                {errors.confirmPassword && (
+                  <p className="text-red-500 text-sm">
+                    {errors.confirmPassword}
+                  </p>
+                )}
+              </form>
+            </div>
 
-        <input type="file" name="profile" onChange={handleFileChange} />
-        {errors.profile && <p className="error-message">{errors.profile}</p>}
+            <div className="flex flex-col gap-4">
+              <label className="font-medium text-lg">Email:</label>
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                value={formData.email}
+                onChange={handleChange}
+                className="placeholder:text-gray-500 placeholder:tracking-wide placeholder:text-base p-3 border bg-white bg-opacity-40 backdrop-blur-lg border-gray-200 shadow-lg rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+              {errors.email && (
+                <p className="text-red-500 text-sm">{errors.email}</p>
+              )}
+              <label className="font-medium text-lg">Profile Picture:</label>
+              <input
+                type="file"
+                name="profile"
+                accept="image/png, image/jpeg"
+                onChange={handleFileChange}
+                className=" p-3 border bg-white bg-opacity-40 backdrop-blur-lg border-gray-200 shadow-lg rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+              {errors.profile && (
+                <p className="text-red-500 text-sm">{errors.profile}</p>
+              )}
+<div className="flex flex-col items-center gap-2">
+              <button
+                type="button"
+                className="mt-6 bg-blue-800 text-white py-3 px-4 w-80 justify-center rounded-lg font-bold transform hover:scale-105 transition duration-300 shadow-lg"
+                onClick={handleSubmit} 
+              >
+                Register
+              </button>
 
-        <button type="submit">Register</button>
-      </form>
+              <button
+                type="button"
+                className="mt-1 bg-white w-80 text-blue-800 py-3 rounded-lg font-bold flex items-center justify-center shadow-lg border transform hover:scale-105 transition duration-300"
+                onClick={handleGoogleSignIn}
+              >
+                <img
+                  src="/assets/images/google-icon.png"
+                  alt="Google Icon"
+                  className="w-6 h-6 mr-2"
+                />
+                Sign in with Google
+              </button>
+              </div>
+
+              <p className="text-center mt-4 text-black text-lg tracking-wide font-semibold">
+                Already have an account?{" "}
+                <a href="/login" className="text-blue-800 font-bold underline tracking-wider">
+                  Login
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
